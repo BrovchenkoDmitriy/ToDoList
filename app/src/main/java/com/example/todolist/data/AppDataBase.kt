44d108fd731e2043e3554402d63deadd4c4f274d
BrovchenkoDmitriy <99586855+BrovchenkoDmitriy@users.noreply.github.com>
2024-01-12 16:15:51 +0900

    Data Task: - work with AppDataBase.kt - work with RepositoryImpl.kt - work with TaskItemDao.kt - work with TaskItemDbModel.kt - add implementations for Room - create TaskItemMapper.kt

--- app/src/main/java/com/example/todolist/data/AppDataBase.kt
+++ app/src/main/java/com/example/todolist/data/AppDataBase.kt
@@ -1,4 +1,36 @@
 package com.example.todolist.data
 
-class AppDataBase {
+import android.app.Application
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [TaskItemDbModel::class], version = 1)
+abstract class AppDataBase :RoomDatabase() {
+    abstract fun TaskItemDao():TaskItemDao
+
+    companion object{
+        private const val DB_NAME = "task_item_db"
+        private var INSTANCE: AppDataBase? = null
+        private val LOCK = Any()
+
+
+        fun getInstance(application: Application): AppDataBase {
+            INSTANCE?.let {
+                return it
+            }
+            synchronized(LOCK) {
+                INSTANCE?.let {
+                    return it
+                }
+                val db = Room.databaseBuilder(
+                    application,
+                    AppDataBase::class.java,
+                    DB_NAME
+                ).build()
+                INSTANCE = db
+                return db
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/todolist/data/TaskItemDbModel.kt
+++ app/src/main/java/com/example/todolist/data/TaskItemDbModel.kt
@@ -1,4 +1,16 @@
 package com.example.todolist.data
 
-class TaskItemDbModel {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "task_items")
+data class TaskItemDbModel(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int,
+    @ColumnInfo(name = "dateStart")
+    val dateStart: Long,
+    val dateFinish: Long,
+    val name: String,
+    val description: String
+    )
